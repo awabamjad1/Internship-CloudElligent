74b1d10c9c28ba259fdd33df76367f3f3f9213fb
awabamjad1 <awab.amjad@cloudelligent.com>
2023-11-07 22:44:35 +0500

    Wrote infra code for spinning up an EC2 instance

---
+++ Sprint2/07-11-2023/infra/iam.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "ec2" {
+  name = "ec2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_policy" "ec2" {
+  name        = "ec2"
+
+  policy = jsonencode({
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1699376838534",
+      "Action": "ec2:*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+})
+}
+resource "aws_iam_role_policy_attachment" "ecr_role_attachment" {
+  policy_arn = aws_iam_policy.ec2.arn
+  role       = aws_iam_role.ec2.name
+}
+resource "aws_iam_instance_profile" "ec2" {
+  name = "EC2"
+  role = aws_iam_role.ec2.name
+}
